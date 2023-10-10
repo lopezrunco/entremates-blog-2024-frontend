c8f6f21a39cefc095ef2d6a2a2a107e50c7e1619
Damián López <lopezrunco@gmail.com>
2023-10-10 13:15:45 +0530

    CSS cleaned, favicon added, basic Header and Navigation components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Col, Container, Row } from 'reactstrap';
+
+export interface IHeaderProps {
+    height?: string;
+    image?: string;
+    title: string;
+    headline: string;
+    children?: any
+}
+
+const Header: React.FunctionComponent<IHeaderProps> = (props) => {
+    const { children, height, image, headline, title } = props;
+
+    let headerStyle = {
+        background: 'linear-gradient(rgba(36, 20, 38, 0.5), rgba(36, 39, 38, 0.5)), url(' + image + ') no-repeat center center',
+        WebkitBackgroundSize: 'cover',
+        MozBackgroundSize: 'cover',
+        OBackgroundSize: 'cover',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        width: '100%',
+        height: height
+    };
+
+    return (
+        <header style={headerStyle}>
+            <Container>
+                <Row className="align-items-center text-center">
+                    <Col>
+                        <h1 className="display-4 text-white mt-5 mb-2">{title}</h1>
+                        <h3 className="mb-5 text-white">{headline}</h3>
+                        {children}
+                    </Col>
+                </Row>
+            </Container>
+        </header>
+    );
+};
+
+Header.defaultProps = {
+    height: '100%',
+    image: 'https://entrematesyotrasyerbas.com.uy/img/mate-bg-desktop.jpg'
+}
+
+export default Header;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,20 @@
+import { Container } from 'reactstrap';
+
 import IPageProps from '../interfaces/page';
 
+import Navigation from '../components/Navigation';
+import Header from '../components/Header';
+
 const HomePage: React.FunctionComponent<IPageProps> = (props) => {
-    return <p>Home page</p>;
+    return (
+        <Container fluid className="p-0">
+            <Navigation />
+            <Header title="Entre mates y otras yerbas" headline="Fe - Esperanza - Caridad" />
+            <Container className="mt-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </Container>
+        </Container>
+    );
 };
 
 export default HomePage;
