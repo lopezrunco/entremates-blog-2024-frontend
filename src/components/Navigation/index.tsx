66e22ff6178076d4c2d2542f865310e9c1cad7aa
membrillo amargo <lopezrunco@gmail.com>
2023-11-08 10:21:07 +0530

    basic nav working in desktop & mobile

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { Navbar, Container, Nav, NavbarText, Button, NavItem, NavLink, NavbarToggler, Collapse } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Navbar, Container, NavbarBrand, Nav, NavbarText, Button } from 'reactstrap';
+
 import UserContext, { initialUserState } from '../../contexts/user';
 
 export interface INavigationProps {}
@@ -8,6 +9,11 @@ export interface INavigationProps {}
 const Navigation: React.FunctionComponent<INavigationProps> = (props) => {
     const userContext = useContext(UserContext);
     const { user } = userContext.userState;
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
 
     const Logout = () => {
         userContext.userDispatch({
@@ -19,25 +25,47 @@ const Navigation: React.FunctionComponent<INavigationProps> = (props) => {
     return (
         <Navbar color="light" light sticky="top" expand="md">
             <Container>
-                <NavbarBrand tag={Link} to="/">
-                    EMYOY
-                </NavbarBrand>
-                <Nav className="mr-auto" navbar />
-                {user._id === '' ? (
-                    <NavbarText tag={Link} to="/login">
-                        Login
-                    </NavbarText>
-                ) : (
-                    <>
-                        <Button outline tag={Link} to="/edit">
-                            Post a Blog
-                        </Button>
-                        <NavbarText className="mr-2 ml-2">\</NavbarText>
-                        <Button outline size="sm" onClick={() => Logout()}>
-                            Logout
-                        </Button>
-                    </>
-                )}
+                <NavbarToggler onClick={toggleNavbar} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink tag={Link} to="/">
+                                Inicio
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/revista">
+                                Revista
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/novedades">
+                                Novedades
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/contacto">
+                                Contacto
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+
+                    {user._id === '' ? (
+                        <NavbarText tag={Link} to="/login">
+                            Administración
+                        </NavbarText>
+                    ) : (
+                        <>
+                            <Button outline tag={Link} to="/edit">
+                                Crear artículo
+                            </Button>
+                            <NavbarText className="mr-2 ml-2">\</NavbarText>
+                            <Button outline size="sm" onClick={() => Logout()}>
+                                Cerrar sesión
+                            </Button>
+                        </>
+                    )}
+                </Collapse>
             </Container>
         </Navbar>
     );
